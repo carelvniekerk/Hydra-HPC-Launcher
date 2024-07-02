2c5fb9059dca14ab9b420666ac7ccddcaa522aa5
Carel van Niekerk <cvanniekerk@hhu.de>
2024-07-02 13:07:38 +0200

    Refactor main test script to use the new input module.

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
-from hydra.conf import HydraConf
-from hydra_zen import ZenStore, builds, zen
+from hydra_zen import store, zen
 
-from hydra_plugins.hpc_submission_launcher.launcher import HPCSubmissionLauncher
+from hydra_plugins.hpc_submission_launcher.launcher import (
+    HPCSubmissionLauncher,  # noqa: F401
+)
 
 
+@store(
+    name="main",
+    hydra_defaults=["_self_", {"override hydra/launcher": "hpc_submission"}],
+)
 def train_model(learning_rate: float = 0.01, batch_size: int = 32, epochs: int = 10):
     print(
         f"Training model with learning_rate={learning_rate}, batch_size={batch_size}, epochs={epochs}"
     )
-    # Your actual training code here
-
-
-ModelConfig = builds(train_model, populate_full_signature=True)
-
-store = ZenStore()
-store(ModelConfig, name="main")
 
 
 if __name__ == "__main__":
-    hydra_conf = HydraConf(
-        launcher={
-            "_target_": "hydra_plugins.hpc_submission_launcher.launcher.HPCSubmissionLauncher",
-        },
-    )
-    store(hydra_conf)
     store.add_to_hydra_store()
 
     zen(train_model).hydra_main(
